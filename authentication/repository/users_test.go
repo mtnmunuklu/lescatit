5ccdc04efed1ef187999970d63662c976711a455
metinmunuklu <mtnmunuklu@gmail.com>
2021-06-25 16:49:23 +0300

    Edited user test and added password test

--- authentication/repository/users_test.go
+++ authentication/repository/users_test.go
@@ -188,3 +188,28 @@ func TestUsersRepositoryDelete(t *testing.T) {
 	assert.EqualError(t, mgo.ErrNotFound, err.Error())
 	assert.Nil(t, found)
 }
+func TestUsersRepositoryGetAll(t *testing.T) {
+	cfg := db.NewConfig()
+	conn, err := db.NewConnection(cfg)
+	assert.NoError(t, err)
+	defer conn.Close()
+
+	id := bson.NewObjectId()
+
+	user := &models.User{
+		Id:       id,
+		Name:     "TEST",
+		Email:    fmt.Sprintf("%s@email.test", id.Hex()),
+		Password: "123456789",
+		Created:  time.Now(),
+		Updated:  time.Now(),
+	}
+
+	r := NewUsersRepository(conn)
+	err = r.Save(user)
+	assert.NoError(t, err)
+
+	users, err := r.GetAll()
+	assert.NoError(t, err)
+	assert.NotEmpty(t, users)
+}
---
+++ security/password_test.go
@@ -0,0 +1,25 @@
+package security
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEncryptPassword(t *testing.T) {
+	pass, err := EncryptPassword("123456789")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, pass)
+	assert.Len(t, pass, 60)
+}
+
+func TestVerifyPassword(t *testing.T) {
+	pass, err := EncryptPassword("123456789")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, pass)
+
+	assert.NoError(t, VerifyPassword(pass, "123456789"))
+	assert.Error(t, VerifyPassword(pass, "1234567"))
+	assert.Error(t, VerifyPassword(pass, pass))
+	assert.Error(t, VerifyPassword("123456789", pass))
+}
