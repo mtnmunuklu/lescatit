3d5d89619dca5f2e75a022daa39aa14495ba8024
metinmunuklu <mtnmunuklu@gmail.com>
2021-07-19 18:31:13 +0300

    Added categorization service build

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 go clean --cache && go test -v -cover CWS/...
 go build -o authentication/authsvc authentication/main.go
 go build -o api/apisvc api/main.go
+go build -o categorization/catsvc categorization/main.go
