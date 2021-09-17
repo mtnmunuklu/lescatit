1f2be9066c531a47da2b7f231a29e86e2c8fda02
metinmunuklu <mtnmunuklu@gmail.com>
2021-09-17 18:13:27 +0300

    Categorizer service edited

--- categorizer/service/service.go
+++ categorizer/service/service.go
@@ -19,19 +19,26 @@ func NewCatzeService(categorizersRepository repository.CategorizersRepository) p
 
 // CategorizeURL provides to categorize the url.
 func (s *CatzeService) CategorizeURL(ctx context.Context, req *pb.CategorizeURLRequest) (*pb.CategorizeURLResponse, error) {
+	err := validators.ValidateURL(req.GetUrl())
+	if err != nil {
+		return nil, err
+	}
 	if len(req.Data) > 0 {
-		return &pb.CategorizeURLResponse{Url: req.Url, Category: "uncategorized"}, nil
+		return &pb.CategorizeURLResponse{Url: req.GetUrl(), Category: "uncategorized"}, nil
 	}
 	return nil, validators.ErrEmptyData
 }
 
 // CategorizeURLs provides to categorize the urls.
 func (s *CatzeService) CategorizeURLs(req *pb.CategorizeURLsRequest, stream pb.CatzeService_CategorizeURLsServer) error {
-	for _, url := range req.CategorizeURLRequest {
-		if len(url.Data) > 0 {
-			err := stream.Send(&pb.CategorizeURLResponse{Url: url.Url, Category: "uncategorized"})
-			if err != nil {
-				return err
+	for _, url := range req.CategorizeURLsRequest {
+		err := validators.ValidateURL(url.GetUrl())
+		if err == nil {
+			if len(url.GetData()) > 0 {
+				err := stream.Send(&pb.CategorizeURLResponse{Url: url.GetUrl(), Category: "uncategorized"})
+				if err != nil {
+					return err
+				}
 			}
 		}
 	}
--- categorizer/validators/validators.go
+++ categorizer/validators/validators.go
@@ -1,8 +1,21 @@
 package validators
 
-import "errors"
+import (
+	"errors"
+	"net/url"
+)
 
 // Contains error codes for categorizer service.
 var (
-	ErrEmptyData = errors.New("data can't be empty")
+	ErrEmptyData  = errors.New("data can't be empty")
+	ErrInvalidURL = errors.New("invalid url")
 )
+
+// ValidateURLs validates if it's a real url.
+func ValidateURL(reqURL string) error {
+	_, err := url.ParseRequestURI(reqURL)
+	if err != nil {
+		return ErrInvalidURL
+	}
+	return nil
+}
