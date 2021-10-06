8ac914e1c731eb072a24a11d5cff2342e8df5796
metinmunuklu <mtnmunuklu@gmail.com>
2021-10-06 22:46:34 +0300

    Added api crawler test

---
+++ api/tests/crawl_test.go
@@ -0,0 +1,290 @@
+package tests
+
+import (
+	"Lescatit/pb"
+	"encoding/json"
+	"flag"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	crawlAddr string
+)
+
+func init() {
+	flag.StringVar(&crawlAddr, "crawl_addr", "http://localhost:9000", "crawler urld address")
+}
+
+// TestGetURLData tests retrieving the content in the url address.
+func TestGetURLData(t *testing.T) {
+	//get token
+	url := crawlAddr + "/signin"
+	jsonSignIn := map[string]interface{}{
+		"Name":     "New Test User",
+		"Email":    "testuser@email.com",
+		"Password": "testuser",
+	}
+	jsonSignInByte, err := json.Marshal(jsonSignIn)
+	assert.NoError(t, err)
+	payload := strings.NewReader(string(jsonSignInByte))
+
+	client := &http.Client{}
+	request, err := http.NewRequest("POST", url, payload)
+	assert.NoError(t, err)
+	assert.NotNil(t, request)
+
+	request.Header.Add("Content-Type", "application/json")
+	response, err := client.Do(request)
+	assert.NoError(t, err)
+	assert.NotNil(t, response)
+
+	body, err := ioutil.ReadAll(response.Body)
+	assert.NoError(t, err)
+	assert.NotNil(t, body)
+
+	signInResponse := new(pb.SignInResponse)
+	err = json.Unmarshal(body, signInResponse)
+	assert.NoError(t, err)
+	assert.NotNil(t, signInResponse)
+	assert.NotNil(t, signInResponse.GetToken())
+	assert.Equal(t, jsonSignIn["Name"], signInResponse.User.GetName())
+	assert.Equal(t, jsonSignIn["Email"], signInResponse.User.GetEmail())
+
+	// get data of the url
+	url = crawlAddr + "/url_data"
+	request, err = http.NewRequest("GET", url, nil)
+	assert.NoError(t, err)
+	assert.NotNil(t, request)
+
+	authorization := "Bearer" + signInResponse.GetToken()
+	request.Header.Add("Authorization", authorization)
+	request.Header.Add("Url", "https://sozcu.com.tr/")
+	response, err = client.Do(request)
+	assert.NoError(t, err)
+	assert.NotNil(t, response)
+
+	body, err = ioutil.ReadAll(response.Body)
+	assert.NoError(t, err)
+	assert.NotNil(t, body)
+
+	getURLData := new(pb.GetURLDataResponse)
+	err = json.Unmarshal(body, getURLData)
+	assert.NoError(t, err)
+	assert.NotNil(t, getURLData)
+	assert.NotEmpty(t, getURLData.GetUrl())
+	assert.NotEmpty(t, getURLData.GetData())
+}
+
+// TestGetURLsData tests retrieving the content in the url addresses.
+func TestGetURLsData(t *testing.T) {
+	//get token
+	url := crawlAddr + "/signin"
+	jsonSignIn := map[string]interface{}{
+		"Name":     "New Test User",
+		"Email":    "testuser@email.com",
+		"Password": "testuser",
+	}
+	jsonSignInByte, err := json.Marshal(jsonSignIn)
+	assert.NoError(t, err)
+	payload := strings.NewReader(string(jsonSignInByte))
+
+	client := &http.Client{}
+	request, err := http.NewRequest("POST", url, payload)
+	assert.NoError(t, err)
+	assert.NotNil(t, request)
+
+	request.Header.Add("Content-Type", "application/json")
+	response, err := client.Do(request)
+	assert.NoError(t, err)
+	assert.NotNil(t, response)
+
+	body, err := ioutil.ReadAll(response.Body)
+	assert.NoError(t, err)
+	assert.NotNil(t, body)
+
+	signInResponse := new(pb.SignInResponse)
+	err = json.Unmarshal(body, signInResponse)
+	assert.NoError(t, err)
+	assert.NotNil(t, signInResponse)
+	assert.NotNil(t, signInResponse.GetToken())
+	assert.Equal(t, jsonSignIn["Name"], signInResponse.User.GetName())
+	assert.Equal(t, jsonSignIn["Email"], signInResponse.User.GetEmail())
+
+	// get data of the urls
+	url = crawlAddr + "/urls_data"
+	request, err = http.NewRequest("GET", url, nil)
+	assert.NoError(t, err)
+	assert.NotNil(t, request)
+
+	authorization := "Bearer" + signInResponse.GetToken()
+	request.Header.Add("Authorization", authorization)
+	request.Header.Add("Urls", "https://sozcu.com.tr/,https://www.haberler.com/")
+	response, err = client.Do(request)
+	assert.NoError(t, err)
+	assert.NotNil(t, response)
+
+	body, err = ioutil.ReadAll(response.Body)
+	assert.NoError(t, err)
+	assert.NotNil(t, body)
+
+	getURLsData := new([]pb.GetURLDataResponse)
+	err = json.Unmarshal(body, getURLsData)
+	assert.NoError(t, err)
+	assert.NotNil(t, getURLsData)
+}
+
+// TestCrawlURL tests crawl the url
+func TestCrawlURL(t *testing.T) {
+	//get token
+	url := crawlAddr + "/signin"
+	jsonSignIn := map[string]interface{}{
+		"Name":     "New Test User",
+		"Email":    "testuser@email.com",
+		"Password": "testuser",
+	}
+	jsonSignInByte, err := json.Marshal(jsonSignIn)
+	assert.NoError(t, err)
+	payload := strings.NewReader(string(jsonSignInByte))
+
+	client := &http.Client{}
+	request, err := http.NewRequest("POST", url, payload)
+	assert.NoError(t, err)
+	assert.NotNil(t, request)
+
+	request.Header.Add("Content-Type", "application/json")
+	response, err := client.Do(request)
+	assert.NoError(t, err)
+	assert.NotNil(t, response)
+
+	body, err := ioutil.ReadAll(response.Body)
+	assert.NoError(t, err)
+	assert.NotNil(t, body)
+
+	signInResponse := new(pb.SignInResponse)
+	err = json.Unmarshal(body, signInResponse)
+	assert.NoError(t, err)
+	assert.NotNil(t, signInResponse)
+	assert.NotNil(t, signInResponse.GetToken())
+	assert.Equal(t, jsonSignIn["Name"], signInResponse.User.GetName())
+	assert.Equal(t, jsonSignIn["Email"], signInResponse.User.GetEmail())
+
+	// crawl url
+	url = crawlAddr + "/url_crawl"
+	jsonCrawlURL := map[string]interface{}{
+		"Url": "https://sozcu.com.tr/",
+		"CrawlRequest": map[string]interface{}{
+			"UserAgent":            "colly - https://github.com/gocolly/colly",
+			"MaxDepth":             0,
+			"AllowedDomains":       []string{},
+			"DisallowedDomains":    []string{},
+			"DisallowedUrlFilters": []string{},
+			"UrlFlters":            []string{},
+			"UrlRevisit":           false,
+			"MaxBodySize":          0,
+			"RobotsTxt":            true,
+		},
+	}
+	jsonCrawlURLByte, err := json.Marshal(jsonCrawlURL)
+	assert.NoError(t, err)
+	payload = strings.NewReader(string(jsonCrawlURLByte))
+	request, err = http.NewRequest("POST", url, payload)
+	assert.NoError(t, err)
+	assert.NotNil(t, request)
+
+	authorization := "Bearer" + signInResponse.GetToken()
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Authorization", authorization)
+	response, err = client.Do(request)
+	assert.NoError(t, err)
+	assert.NotNil(t, response)
+
+	body, err = ioutil.ReadAll(response.Body)
+	assert.NoError(t, err)
+	assert.NotNil(t, body)
+
+	getURLsData := new([]pb.GetURLDataResponse)
+	err = json.Unmarshal(body, getURLsData)
+	assert.NoError(t, err)
+	assert.NotNil(t, getURLsData)
+
+}
+
+// TestCrawlURLs tests crawl the urls
+func TestCrawlURLs(t *testing.T) {
+	//get token
+	url := crawlAddr + "/signin"
+	jsonSignIn := map[string]interface{}{
+		"Name":     "New Test User",
+		"Email":    "testuser@email.com",
+		"Password": "testuser",
+	}
+	jsonSignInByte, err := json.Marshal(jsonSignIn)
+	assert.NoError(t, err)
+	payload := strings.NewReader(string(jsonSignInByte))
+
+	client := &http.Client{}
+	request, err := http.NewRequest("POST", url, payload)
+	assert.NoError(t, err)
+	assert.NotNil(t, request)
+
+	request.Header.Add("Content-Type", "application/json")
+	response, err := client.Do(request)
+	assert.NoError(t, err)
+	assert.NotNil(t, response)
+
+	body, err := ioutil.ReadAll(response.Body)
+	assert.NoError(t, err)
+	assert.NotNil(t, body)
+
+	signInResponse := new(pb.SignInResponse)
+	err = json.Unmarshal(body, signInResponse)
+	assert.NoError(t, err)
+	assert.NotNil(t, signInResponse)
+	assert.NotNil(t, signInResponse.GetToken())
+	assert.Equal(t, jsonSignIn["Name"], signInResponse.User.GetName())
+	assert.Equal(t, jsonSignIn["Email"], signInResponse.User.GetEmail())
+
+	// crawl url
+	url = crawlAddr + "/url_crawl"
+	jsonCrawlURL := map[string]interface{}{
+		"Url": []string{"https://sozcu.com.tr/", "https://www.haberler.com/"},
+		"CrawlRequest": map[string]interface{}{
+			"UserAgent":            "colly - https://github.com/gocolly/colly",
+			"MaxDepth":             0,
+			"AllowedDomains":       []string{},
+			"DisallowedDomains":    []string{},
+			"DisallowedUrlFilters": []string{},
+			"UrlFlters":            []string{},
+			"UrlRevisit":           false,
+			"MaxBodySize":          0,
+			"RobotsTxt":            true,
+		},
+	}
+	jsonCrawlURLByte, err := json.Marshal(jsonCrawlURL)
+	assert.NoError(t, err)
+	payload = strings.NewReader(string(jsonCrawlURLByte))
+	request, err = http.NewRequest("POST", url, payload)
+	assert.NoError(t, err)
+	assert.NotNil(t, request)
+
+	authorization := "Bearer" + signInResponse.GetToken()
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Authorization", authorization)
+	response, err = client.Do(request)
+	assert.NoError(t, err)
+	assert.NotNil(t, response)
+
+	body, err = ioutil.ReadAll(response.Body)
+	assert.NoError(t, err)
+	assert.NotNil(t, body)
+
+	getURLsData := new([]pb.GetURLDataResponse)
+	err = json.Unmarshal(body, getURLsData)
+	assert.NoError(t, err)
+	assert.NotNil(t, getURLsData)
+}
