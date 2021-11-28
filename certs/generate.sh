4bf4fde64c14dab00cf7345847b1f42023adaeb6
metinmunuklu <mtnmunuklu@gmail.com>
2021-11-28 11:49:25 +0300

    Updated certificates

--- certs/generate.sh
+++ certs/generate.sh
@@ -17,8 +17,7 @@ openssl req -newkey rsa:4096 -nodes -keyout server-key.pem \
 # 3. Use CA's private key to sign web server's CSR and get back the signed certificate
 openssl x509 -req -in server-req.pem  \
     -days 60 -CA ca-cert.pem \
-    -CAkey ca-key.pem -CAcreateserial \ 
-    -out server-cert.pem -extfile server-ext.cnf
+    -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -extfile server-ext.cnf
 
 echo "Server's signed certificate"
 openssl x509 -in server-cert.pem -noout -text
