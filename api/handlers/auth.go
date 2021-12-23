e487c69cb2cb81bf8d53cf6d7a0fdd44d0f86162
metinmunuklu <mtnmunuklu@gmail.com>
2021-12-23 13:40:10 +0300

    Edited authentication handler in api

--- api/handlers/auth.go
+++ api/handlers/auth.go
@@ -38,23 +38,27 @@ func (h *AHandlers) SignUp(w http.ResponseWriter, r *http.Request) {
 		util.WriteError(w, http.StatusBadRequest, util.ErrEmptyBody)
 		return
 	}
+
 	defer r.Body.Close()
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		util.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
+
 	signUpRequest := new(pb.SignUpRequest)
 	err = json.Unmarshal(body, signUpRequest)
 	if err != nil {
 		util.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
+
 	signUp, err := h.authSvcClient.SignUp(r.Context(), signUpRequest)
 	if err != nil {
 		util.WriteError(w, http.StatusUnprocessableEntity, err)
 		return
 	}
+
 	util.WriteAsJson(w, http.StatusCreated, signUp)
 }
 
@@ -64,23 +68,27 @@ func (h *AHandlers) SignIn(w http.ResponseWriter, r *http.Request) {
 		util.WriteError(w, http.StatusBadRequest, util.ErrEmptyBody)
 		return
 	}
+
 	defer r.Body.Close()
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		util.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
+
 	signInRequest := new(pb.SignInRequest)
 	err = json.Unmarshal(body, signInRequest)
 	if err != nil {
 		util.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
+
 	signIn, err := h.authSvcClient.SignIn(r.Context(), signInRequest)
 	if err != nil {
 		util.WriteError(w, http.StatusUnprocessableEntity, err)
 		return
 	}
+
 	util.WriteAsJson(w, http.StatusOK, signIn)
 }
 
@@ -92,25 +100,30 @@ func (h *AHandlers) GetUser(w http.ResponseWriter, r *http.Request) {
 		util.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
+
 	getedUserRole, err := h.authSvcClient.GetUserRole(r.Context(), &pb.GetUserRoleRequest{Id: userId})
 	if err != nil {
 		util.WriteError(w, http.StatusUnprocessableEntity, err)
 		return
 	}
+
 	userIsAdmin := util.CheckUserIsAdmin(getedUserRole.Role)
 	if !userIsAdmin {
 		util.WriteError(w, http.StatusUnauthorized, util.ErrUnauthorized)
 		return
 	}
+
 	// get user
 	email := r.Header.Get("Email")
 	user := new(pb.GetUserRequest)
 	user.Email = email
+
 	getedUser, err := h.authSvcClient.GetUser(r.Context(), user)
 	if err != nil {
 		util.WriteError(w, http.StatusUnprocessableEntity, err)
 		return
 	}
+
 	util.WriteAsJson(w, http.StatusOK, getedUser)
 }
 
@@ -122,25 +135,30 @@ func (h *AHandlers) DeleteUser(w http.ResponseWriter, r *http.Request) {
 		util.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
+
 	getedUserRole, err := h.authSvcClient.GetUserRole(r.Context(), &pb.GetUserRoleRequest{Id: userId})
 	if err != nil {
 		util.WriteError(w, http.StatusUnprocessableEntity, err)
 		return
 	}
+
 	userIsAdmin := util.CheckUserIsAdmin(getedUserRole.Role)
 	if !userIsAdmin {
 		util.WriteError(w, http.StatusUnauthorized, util.ErrUnauthorized)
 		return
 	}
+
 	// delete user
 	email := r.Header.Get("Email")
 	user := new(pb.DeleteUserRequest)
 	user.Email = email
+
 	deletedUser, err := h.authSvcClient.DeleteUser(r.Context(), user)
 	if err != nil {
 		util.WriteError(w, http.StatusUnprocessableEntity, err)
 		return
 	}
+
 	util.WriteAsJson(w, http.StatusOK, deletedUser)
 }
 
@@ -152,38 +170,45 @@ func (h *AHandlers) ChangeUserRole(w http.ResponseWriter, r *http.Request) {
 		util.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
+
 	getedUserRole, err := h.authSvcClient.GetUserRole(r.Context(), &pb.GetUserRoleRequest{Id: userId})
 	if err != nil {
 		util.WriteError(w, http.StatusUnprocessableEntity, err)
 		return
 	}
+
 	userIsAdmin := util.CheckUserIsAdmin(getedUserRole.Role)
 	if !userIsAdmin {
 		util.WriteError(w, http.StatusUnauthorized, util.ErrUnauthorized)
 		return
 	}
+
 	// change user role
 	if r.Body == nil {
 		util.WriteError(w, http.StatusBadRequest, util.ErrEmptyBody)
 		return
 	}
+
 	defer r.Body.Close()
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		util.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
+
 	user := new(pb.ChangeUserRoleRequest)
 	err = json.Unmarshal(body, user)
 	if err != nil {
 		util.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
+
 	changedUser, err := h.authSvcClient.ChangeUserRole(r.Context(), user)
 	if err != nil {
 		util.WriteError(w, http.StatusUnprocessableEntity, err)
 		return
 	}
+
 	util.WriteAsJson(w, http.StatusOK, changedUser)
 }
 
@@ -193,23 +218,27 @@ func (h *AHandlers) UpdateUserPassword(w http.ResponseWriter, r *http.Request) {
 		util.WriteError(w, http.StatusBadRequest, util.ErrEmptyBody)
 		return
 	}
+
 	defer r.Body.Close()
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		util.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
+
 	updatePasswordRequest := new(pb.UpdateUserPasswordRequest)
 	err = json.Unmarshal(body, updatePasswordRequest)
 	if err != nil {
 		util.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
+
 	updatedUser, err := h.authSvcClient.UpdateUserPassword(r.Context(), updatePasswordRequest)
 	if err != nil {
 		util.WriteError(w, http.StatusUnprocessableEntity, err)
 		return
 	}
+
 	util.WriteAsJson(w, http.StatusOK, updatedUser)
 }
 
@@ -219,23 +248,27 @@ func (h *AHandlers) UpdateUserEmail(w http.ResponseWriter, r *http.Request) {
 		util.WriteError(w, http.StatusBadRequest, util.ErrEmptyBody)
 		return
 	}
+
 	defer r.Body.Close()
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		util.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
+
 	updateEmailRequest := new(pb.UpdateUserEmailRequest)
 	err = json.Unmarshal(body, updateEmailRequest)
 	if err != nil {
 		util.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
+
 	updatedUser, err := h.authSvcClient.UpdateUserEmail(r.Context(), updateEmailRequest)
 	if err != nil {
 		util.WriteError(w, http.StatusUnprocessableEntity, err)
 		return
 	}
+
 	util.WriteAsJson(w, http.StatusOK, updatedUser)
 }
 
@@ -245,23 +278,27 @@ func (h *AHandlers) UpdateUserName(w http.ResponseWriter, r *http.Request) {
 		util.WriteError(w, http.StatusBadRequest, util.ErrEmptyBody)
 		return
 	}
+
 	defer r.Body.Close()
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		util.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
+
 	updateNameRequest := new(pb.UpdateUserNameRequest)
 	err = json.Unmarshal(body, updateNameRequest)
 	if err != nil {
 		util.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
+
 	updatedUser, err := h.authSvcClient.UpdateUserName(r.Context(), updateNameRequest)
 	if err != nil {
 		util.WriteError(w, http.StatusUnprocessableEntity, err)
 		return
 	}
+
 	util.WriteAsJson(w, http.StatusOK, updatedUser)
 }
 
@@ -273,33 +310,40 @@ func (h *AHandlers) GetUsers(w http.ResponseWriter, r *http.Request) {
 		util.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
+
 	getedUserRole, err := h.authSvcClient.GetUserRole(r.Context(), &pb.GetUserRoleRequest{Id: userId})
 	if err != nil {
 		util.WriteError(w, http.StatusUnprocessableEntity, err)
 		return
 	}
+
 	userIsAdmin := util.CheckUserIsAdmin(getedUserRole.Role)
 	if !userIsAdmin {
 		util.WriteError(w, http.StatusUnauthorized, util.ErrUnauthorized)
 		return
 	}
+
 	// get users
 	stream, err := h.authSvcClient.ListUsers(r.Context(), &pb.ListUsersRequest{})
 	if err != nil {
 		util.WriteError(w, http.StatusUnprocessableEntity, err)
 		return
 	}
+
 	var getedUsers []*pb.User
 	for {
 		user, err := stream.Recv()
 		if err == io.EOF {
 			break
 		}
+
 		if err != nil {
 			util.WriteError(w, http.StatusBadRequest, err)
 			return
 		}
+
 		getedUsers = append(getedUsers, user)
 	}
+
 	util.WriteAsJson(w, http.StatusOK, getedUsers)
 }
