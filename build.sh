9c2132c4b67fd2a64e85b0bbfa0c9fad12bd21a9
metinmunuklu <mtnmunuklu@gmail.com>
2022-01-22 11:32:41 +0300

    Updated service build script

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#go clean --cache && go test -v -cover ./security/... ./authentication/... ./crawler/... ./categorizer/... ./categorization/...
-go build -o authentication/authsvc authentication/main.go
-go build -o crawler/crawlsvc crawler/main.go
-go build -o categorizer/catzesvc categorizer/main.go
-go build -o categorization/catsvc categorization/main.go
-go build -o api/apisvc api/main.go
