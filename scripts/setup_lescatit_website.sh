39a5eef66f278696b0bf0f3cd8c78dba4b41c629
metinmunuklu <mtnmunuklu@gmail.com>
2022-02-08 09:40:26 +0300

    Added lescatit website setup

---
+++ scripts/setup_lescatit_website.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Download LescatitSite from github release and extract to the /var/www directory
+echo -e "${GREEN}Download LescatitSite from github release${ENDCOLOR}"
+LATEST_RELEASE=$(curl -s https://api.github.com/repos/mtnmunuklu/LescatitSite/releases/latest \
+| grep browser_download_url \
+| grep tar.gz \
+| cut -d '"' -f 4)
+curl -LJO $LATEST_RELEASE
+
+echo -e "${GREEN}extract to the /var/www directory${ENDCOLOR}"
+FILE_NAME=$(echo $LATEST_RELEASE | cut -d '"' -f 9)
+tar -xvf $FILE_NAME -C /var/www/
+
+# Install Hugo
+echo -e "${GREEN}Install Hugo${ENDCOLOR}"
+echo '[daftaupe-hugo]
+name=Copr repo for hugo owned by daftaupe
+baseurl=https://copr-be.cloud.fedoraproject.org/results/daftaupe/hugo/epel-7-$basearch/
+type=rpm-md
+skip_if_unavailable=True
+gpgcheck=1
+gpgkey=https://copr-be.cloud.fedoraproject.org/results/daftaupe/hugo/pubkey.gpg
+repo_gpgcheck=0
+enabled=1
+enabled_metadata=1' | sudo tee -a /etc/yum.repos.d/CentOS-hugo.repo > /dev/null
+yum install hugo
+
+# Run hugo command and create public file
+echo -e "${GREEN}Run hugo command and create public file${ENDCOLOR}"
+cd /var/www/LescatitSite
+hugo
